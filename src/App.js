03656206db175cc5a324152752e5a71d629c2ed8
aditya <aditya.sharma9827.as@gmail.com>
2021-07-14 09:16:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [state, setstate] = useState({
+
+        fname: "",
+        lname: '',
+        email: ''
+
+    });
+    const change = (event) => {
+
+
+        let value = event.target.value;
+        let name = event.target.name;
+
+        setstate((prevalue) => {
+            // if (name === 'fname') {
+
+            //     return {
+            //         fname: value,
+            //         lname: prevalue.lname,
+            //         email: prevalue.email
+            //     };
+            // }
+            // else if (name === 'lname') {
+
+            //     return {
+            //         fname: prevalue.fname,
+            //         lname: value,
+            //         email: prevalue.email
+            //     }
+            // } else if (name === 'email') {
+
+            //     return {
+            //         fname: prevalue.fname,
+            //         lname: prevalue.lname,
+            //         email: value,
+            //     }
+
+            // }
+
+            return {
+                ...prevalue,
+
+                [name]: value,
+
+            }
+
+
+        })
+    }
+
+    const onsubmit = (event) => {
+
+        event.preventDefault();
+
+        alert("form is submmited");
+    }
+
+    return (
+
+        <div className="main" >
+
+            <form onSubmit={onsubmit}>
+                <div className="center">
+
+
+                    <input type="text" placeholder="Enter name" onChange={change} value={state.fname} name="fname" />
+                    <input type="text" placeholder="Enter sname" onChange={change} value={state.lname} name="lname" />
+                    <input type="email" placeholder="Enter sname" onChange={change} value={state.email} name="email" />
+
+
+
+                    <input type="submit"></input>
+
+                    <div className="ans">{state.fname} {state.lname} {state.email}</div>
+
+
+
+                </div>
+            </form>
+        </div>
+
+
+    );
+
 }
-
 export default App;
+
+
+
